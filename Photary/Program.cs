e236ee1dcf45a46791a0f548abbc37fa12617c43
Fuki Sato <lego.sato.4135@gmail.com>
2023-10-02 19:26:33 +0900

    update release path

--- Photary/Program.cs
+++ Photary/Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 #if DEBUG
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5236/") });
 #else
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://www.sato-home.mydns.jp:8443/api/v1/") });
 #endif
 
 await builder.Build().RunAsync();
