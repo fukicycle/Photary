02ab25b99259f315ff0068c31f6f063d4b63664e
Fuki Sato <lego.sato.4135@gmail.com>
2023-10-02 19:12:01 +0900

    remove some files

--- Photary/Program.cs
+++ Photary/Program.cs
@@ -5,8 +5,11 @@ using Photary;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+#if DEBUG
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5236/") });
+#else
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+#endif
 
 await builder.Build().RunAsync();
 
