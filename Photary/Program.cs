fa2106086d7501b62295a89119c456328f63f971
Fuki Sato <lego.sato.4135@gmail.com>
2023-10-03 21:53:17 +0900

    update port number

--- Photary/Program.cs
+++ Photary/Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 #if DEBUG
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5236/") });
 #else
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://www.sato-home.mydns.jp:8443/api/v1/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://www.sato-home.mydns.jp:9443/api/v1/") });
 #endif
 
 await builder.Build().RunAsync();
