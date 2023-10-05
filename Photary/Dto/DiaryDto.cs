e347310a653ee3d4e89b152ca9913601518d1238
Fuki Sato <lego.sato.4135@gmail.com>
2023-10-05 23:13:14 +0900

    implement add functions

---
+++ Photary/Dto/DiaryDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Photary.Dto
+{
+    public class DiaryDto
+    {
+        public string Content { get; set; } = null!;
+    }
+}
+
---
+++ Photary/Dto/DiaryResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Photary.Dto
+{
+	public class DiaryResponseDto
+	{
+        public string Content { get; set; } = null!;
+        public DateTime Date { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
+
